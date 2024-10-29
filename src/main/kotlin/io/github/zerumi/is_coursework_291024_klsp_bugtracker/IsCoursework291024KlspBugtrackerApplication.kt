b07f696eff8bd554e4f1b889cbc87e2770be5035
Zerumi <afanasyev09032005@gmail.com>
2024-10-29 21:40:14 +0300

    feat: init spring boot project

---
+++ src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/IsCoursework291024KlspBugtrackerApplication.kt
@@ -0,0 +1,11 @@
+package io.github.zerumi.is_coursework_291024_klsp_bugtracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IsCoursework291024KlspBugtrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<IsCoursework291024KlspBugtrackerApplication>(*args)
+}
