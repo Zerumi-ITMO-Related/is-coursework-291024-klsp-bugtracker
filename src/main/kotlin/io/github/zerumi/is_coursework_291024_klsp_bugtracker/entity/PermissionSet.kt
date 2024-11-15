5085630301be12abfa916a47d797c24acf166441
Zerumi <afanasyev09032005@gmail.com>
2024-11-16 00:41:38 +0300

    fix: Permission value generation

--- src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/entity/PermissionSet.kt
+++ src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/entity/PermissionSet.kt
@@ -26,23 +26,37 @@ class PermissionSet(
 }
 
 enum class Permissions {
-    PERMISSION_EDIT_PERMISSIONS, PERMISSION_EDIT_COMMENTS,
+    EDIT_PERMISSIONS, CREATE_ISSUE,
     // continue permission set
     ;
 
-    private val value: ULong = Permissions.nextValue
+    private val value: ULong = PermissionValueGenerator.nextValue
 
-    infix fun ULong.or(other: Permissions) = this or other.value
-    infix fun ULong.and(other: Permissions) = this and other.value
+    infix fun or(other: ULong) = this.value or other
+    infix fun and(other: ULong) = this.value and other
     infix fun or(other: Permissions) = this.value or other.value
     infix fun and(other: Permissions) = this.value and other.value
 
+    /*
+    Following code lead to NPE in runtime -- https://youtrack.jetbrains.com/issue/KT-37165
+
     companion object {
-        private var nextValue: ULong = 1u/*
-        get() {
-            field *= 2u
-            return field
-        }
-            */
+        private var nextValue: ULong = 1u
+            get() {
+                field *= 2u
+                return field
+            }
+    }
+    */
+}
+
+private class PermissionValueGenerator {
+    companion object {
+        var nextValue: ULong = 1u
+            get() {
+                field *= 2u
+                return field
+            }
+            private set
     }
 }
