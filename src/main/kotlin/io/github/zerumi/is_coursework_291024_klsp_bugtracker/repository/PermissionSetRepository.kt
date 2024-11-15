9ec6867d564ba8b4592d9c184d5635c4445d877b
Zerumi <afanasyev09032005@gmail.com>
2024-11-15 03:50:25 +0300

    feat: modify jpa repositories

--- src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/repository/PermissionSetRepository.kt
+++ src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/repository/PermissionSetRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PermissionSetRepository : JpaRepository<PermissionSet, Long>
+interface PermissionSetRepository : JpaRepository<PermissionSet, Long> {
+    fun findByName(name: String): PermissionSet?
+}
