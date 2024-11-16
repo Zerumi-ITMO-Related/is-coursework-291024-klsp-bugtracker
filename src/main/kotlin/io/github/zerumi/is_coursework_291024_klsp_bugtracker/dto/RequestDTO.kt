87343ed2dc8b225dc7a2992738d8ececba94f121
Zerumi <afanasyev09032005@gmail.com>
2024-11-16 18:07:07 +0300

    chore: request DTO refactor

--- src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/dto/RequestDTO.kt
+++ src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/dto/RequestDTO.kt
@@ -1,7 +1,9 @@
 package io.github.zerumi.is_coursework_291024_klsp_bugtracker.dto
 
+import java.io.Serializable
+
+data class AuthRequestDTO(val login: String, val password: String) : Serializable
+
 data class IssueRequestDTO(
-    val title: String,
-    val content: String,
-    val attachedFilesIds: List<Long>
-)
+    val title: String, val content: String, val attachedFilesIds: List<Long>
+) : Serializable
