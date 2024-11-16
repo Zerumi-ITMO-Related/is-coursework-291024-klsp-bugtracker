c091c73f732df87322a8cd37e3d88f2aff57a63e
Zerumi <afanasyev09032005@gmail.com>
2024-11-17 01:31:26 +0300

    feat: tags service & controller

---
+++ src/main/kotlin/io/github/zerumi/is_coursework_291024_klsp_bugtracker/controller/TagController.kt
@@ -0,0 +1,49 @@
+package io.github.zerumi.is_coursework_291024_klsp_bugtracker.controller
+
+import io.github.zerumi.is_coursework_291024_klsp_bugtracker.annotation.RequirePermission
+import io.github.zerumi.is_coursework_291024_klsp_bugtracker.dto.TagDTO
+import io.github.zerumi.is_coursework_291024_klsp_bugtracker.dto.TagInfo
+import io.github.zerumi.is_coursework_291024_klsp_bugtracker.dto.TagRequestDTO
+import io.github.zerumi.is_coursework_291024_klsp_bugtracker.dto.toDTO
+import io.github.zerumi.is_coursework_291024_klsp_bugtracker.entity.Permissions
+import io.github.zerumi.is_coursework_291024_klsp_bugtracker.service.TagService
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/tag")
+class TagController(
+    val tagService: TagService
+) {
+    @GetMapping("/{id}")
+    fun getTag(@PathVariable id: Long) : TagDTO {
+        return toDTO(tagService.getById(id))
+    }
+
+    @PostMapping
+    @RequirePermission(Permissions.MANAGE_TAGS)
+    @SecurityRequirement(name = "Bearer Authentication")
+    fun createTag(tagRequestDTO: TagRequestDTO) {
+        return tagService.createTag(tagRequestDTO)
+    }
+
+    @PutMapping
+    @RequirePermission(Permissions.MANAGE_TAGS)
+    @SecurityRequirement(name = "Bearer Authentication")
+    fun updateTag(tagInfo: TagInfo) {
+        return tagService.updateTag(tagInfo)
+    }
+
+    @DeleteMapping
+    @RequirePermission(Permissions.MANAGE_TAGS)
+    @SecurityRequirement(name = "Bearer Authentication")
+    fun deleteTag(tagInfo: TagInfo) {
+        return tagService.deleteTag(tagInfo)
+    }
+}
